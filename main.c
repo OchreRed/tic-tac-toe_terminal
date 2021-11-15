854dc70c3151a7976bfb0f4f9aa8cf361f4de915
OchreRed <92272224+OchreRed@users.noreply.github.com>
2021-11-15 18:05:20 -0500

    Update main.c
    Changed my mind, I'll just make a separate branch or version for tic-tac-toe against a CPU opponent. I know this is kind of unprofessional, but I barely know how to use Github or program. I'll get there.

--- main.c
+++ main.c
@@ -5,93 +5,79 @@
 #define TURN_P1 0
 #define TURN_P2 1
 
-void print_board(char* board, int turn_no, bool player_turn);
-int get_choice(char* board);
-int check_row_win (char a, char b, char c, int ch, char* board);
-int check_board_win(int ch, char* board);
+void print_board(char* board_tiles, int turn_no, bool player_turn);
+char get_choice(char* board_tiles);
+bool row(char a, char b, char c);
+bool check_board(char* board_tiles);
 
-int main (void) {
+int main(void) {
     static int turn_no = 1;
-    static bool player_turn = TURN_P1;
-    static int ch;
-    static int win_check;
-    char board[9] = {'a','b','c','d','e','f','g','h','i'};
-
+    static bool player_turn, game_won;
+    static int choice;
+    char board_tiles[9] = {'a','b','c','d','e','f','g','h','i'};
     while (turn_no < 10) {
-        print_board(board, turn_no, player_turn);
-        ch = get_choice(board);
-        board[ch] = (player_turn == TURN_P1)? 'X' : 'O';
+        print_board(board_tiles, turn_no, player_turn);
+        choice = get_choice(board_tiles);
+        board_tiles[choice] = (player_turn == TURN_P1)? 'X' : 'O';
         player_turn = !player_turn;
         ++turn_no;
-        win_check = check_board_win(ch, board);
-        if (win_check != 0) {
-            print_board(board, turn_no, player_turn);
-            printf("Player %d wins!\n\n", win_check);
+        game_won = check_board(board_tiles);
+        if (game_won) {
+            print_board(board_tiles, turn_no, player_turn);
+            printf("Player %d wins!\n\n", (int)!player_turn + 1);
             return EXIT_SUCCESS;
         }
     }
-    puts("\nAll tiles have been marked, and with no clear winner determined, the game is a draw.\n");
+    puts("All tiles have been marked, and with no clear winner determined, the game is a draw.\n");
     return EXIT_SUCCESS;
 }
 
-void print_board(char* board, int turn_no, bool player_turn) {
+void print_board(char* board_tiles, int turn_no, bool player_turn) {
     printf("\nTurn %d - player %d's turn (you are %c):\n", turn_no, (int)player_turn + 1, (player_turn)? 'O' : 'X');
-    printf(" %c | %c | %c\n", board[0], board[1], board[2]);
-      puts("---+---+---");
-    printf(" %c | %c | %c\n", board[3], board[4], board[5]);
-      puts("---+---+---");
-    printf(" %c | %c | %c\n\n", board[6], board[7], board[8]); 
+    printf(" %c | %c | %c\n", board_tiles[0], board_tiles[1], board_tiles[2]);
+    printf("---+---+---\n");
+    printf(" %c | %c | %c\n", board_tiles[3], board_tiles[4], board_tiles[5]);
+    printf("---+---+---\n");
+    printf(" %c | %c | %c\n\n", board_tiles[6], board_tiles[7], board_tiles[8]); 
     return;
 }
 
-int get_choice(char* board) {
+char get_choice(char* board_tiles) {
     puts("Input the letter of the spot you want to mark.");
     char buf[3]; 
-    int c;
-
+    int len, c;
     input:
-    int len = 0;
+    len = 0;
     while ( (c = getchar()) != '\n') {
         buf[len] = c;
-        if (len > 1)
+        if (len > 1) 
             len = 1;
         ++len;
     }
-    if (len > 1 || !(buf[0] >= 'a' && buf[0] <= 'i')) {
+    if (len > 1 || !(buf[0] >= 'a' && buf[0] <= 'i') || board_tiles[buf[0]-'a'] != buf[0]) {
         puts("Invalid input. Please try again: ");
         goto input;
-    }    
-    int ch = buf[0] - 'a';
-    if (board[ch] == 'X' || board[ch] == 'O') {
-        puts("That spot is taken. Please try again: ");
-        goto input;
     }
-    return ch;
+    return buf[0]-'a';
 }
 
-int check_row_win (char a, char b, char c, int ch, char* board) {
-    if (a==board[ch] && b==board[ch] && c==board[ch]) { 
-        if (board[ch]=='X') return 1;
-        if (board[ch]=='O') return 2;
-    }
-    return 0;
+bool check_row(char a, char b, char c) {
+    return (a == b) && (b == c);
 }
 
-int check_board_win(int ch, char* board) {
-    for (int i = 0; i < 7; i += 3) {
-        int h_win = check_row_win(board[i], board[i+1], board[i+2], ch, board);
-        if (h_win==1) return 1;
-        if (h_win==2) return 2;
+bool check_board(char* board_tiles){
+    bool h_win, v_win, d_win;
+    for (int i = 0; i < 7; i+=3) {
+        h_win = check_row(board_tiles[i], board_tiles[i+1], board_tiles[i+2]);
+        if (h_win) return true;
     }
     for (int i = 0; i < 3; ++i) {
-        int v_win = check_row_win(board[i], board[i+3], board[i+6], ch, board);
-        if (v_win==1) return 1;
-        if (v_win==2) return 2;
+        v_win = check_row(board_tiles[i], board_tiles[i+3], board_tiles[i+6]);
+        if (v_win) return true;
     }
-    for (int i = 0; i < 3 ; i+= 2) {
-        int d_win = check_row_win(board[i], board[4], board[8-i], ch, board);
-        if (d_win==1) return 1;
-        if (d_win==2) return 2;
+    for (int i = 0; i < 3; i+=2) {
+        d_win = check_row(board_tiles[i], board_tiles[4], board_tiles[8-i]);
+        if (d_win) return true;
     }
-    return 0;
+    return false;
 }
